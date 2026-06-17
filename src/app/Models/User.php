d18bf47e0bc6c6260a5c3e4512e4cc44a428276a
nagano tomohiro <n.tomo4649@gmail.com>
2026-06-17 21:32:57 +0900

    feat: メール認証制限を元に戻す

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasFactory, Notifiable;
 
