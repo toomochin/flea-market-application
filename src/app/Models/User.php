e2c42e64e9e20320a6efb4b7dc1f7bc8b78e397e
nagano tomohiro <n.tomo4649@gmail.com>
2026-06-17 18:32:43 +0900

    fix: 本番環境用にメール認証をスキップ

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
